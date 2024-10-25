a62069470ad61ef9a19a092c277b69be0a9e3498
Frederico Schnekenberg <fred.schnekl@gmail.com>
2024-10-25 15:34:26 -0300

    Save command requests cache deletion

--- EssentialFeed2Tests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed2Tests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,24 +6,55 @@
 //
 
 import XCTest
+import EssentialFeed2
 
 class LocalFeedLoaer {
-  init(store: FeedStore) {
+  private let store: FeedStore
 
+  init(store: FeedStore) {
+    self.store = store
+  }
+
+  func save(_ items: [FeedItem]) {
+    store.deleteCacheFeed()
   }
 }
 
 class FeedStore {
   var deleteCacheFeedCallCount = 0
+
+  func deleteCacheFeed() {
+    deleteCacheFeedCallCount += 1
+  }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
 
-    func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        _ = LocalFeedLoaer(store: store)
+  func test_init_doesNotDeleteCacheUponCreation() {
+    let store = FeedStore()
+    _ = LocalFeedLoaer(store: store)
 
-        XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
-    }
+    XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+  }
 
+  func test_save_requestCacheDeletion() {
+    let store = FeedStore()
+    let sut = LocalFeedLoaer(store: store)
+    let items = [uniqueItem(), uniqueItem()]
+
+    sut.save(items)
+
+    XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+  }
+
+}
+
+extension CacheFeedUseCaseTests {
+  private func uniqueItem() -> FeedItem {
+    FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+  }
+
+  private func anyURL() -> URL {
+    URL(string: "http://any-url.com")!
+  }
 }
