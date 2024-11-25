e40cf6206f22c8ee2906c4e29443eff718244c57
Frederico Schnekenberg <fred.schnekl@gmail.com>
2024-11-25 16:32:42 -0300

    Load command requests cache retrieval

--- EssentialFeed2/FeedCache/FeedStore.swift
+++ EssentialFeed2/FeedCache/FeedStore.swift
@@ -10,6 +10,8 @@ import Foundation
 public protocol FeedStore {
   typealias DeletionCompletion = (Error?) -> Void
   typealias InsertionCompletion = (Error?) -> Void
+
   func deleteCacheFeed(completion: @escaping DeletionCompletion)
   func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+  func retrieve()
 }
