54f3b0b692e0db744e8edf4cc901298d5b6592cc
Frederico Schnekenberg <fred.schnekl@gmail.com>
2024-11-26 11:06:55 -0300

    Segment functionalities into extensions

--- EssentialFeed2/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed2/FeedCache/LocalFeedLoader.swift
@@ -22,6 +22,17 @@ public final class LocalFeedLoader {
     self.currentDate = currentDate
   }
 
+  private func validate(_ timestamp: Date) -> Bool {
+    guard let maxAgeCache = calendar.date(
+      byAdding: .day,
+      value: maxCacheAgeInDays,
+      to: timestamp
+    ) else { return false }
+    return currentDate() < maxAgeCache
+  }
+}
+
+extension LocalFeedLoader {
   public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
     store.deleteCacheFeed { [weak self] error in
       guard let self else { return }
@@ -34,6 +45,15 @@ public final class LocalFeedLoader {
     }
   }
 
+  private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
+    store.insert(feed.toLocal(), timestamp: currentDate()) { [weak self] error in
+      guard self != nil else { return }
+      completion(error)
+    }
+  }
+}
+
+extension LocalFeedLoader {
   public func load(completion: @escaping (LoadResult) -> Void) {
     store.retrieve { [weak self] result in
       guard let self else { return }
@@ -47,7 +67,9 @@ public final class LocalFeedLoader {
       }
     }
   }
+}
 
+extension LocalFeedLoader {
   public func validateCache() {
     store.retrieve { [weak self] result in
       guard let self else { return }
@@ -62,22 +84,6 @@ public final class LocalFeedLoader {
       case .empty, .found:
         break
       }
-    }
-  }
-
-  private func validate(_ timestamp: Date) -> Bool {
-    guard let maxAgeCache = calendar.date(
-      byAdding: .day,
-      value: maxCacheAgeInDays,
-      to: timestamp
-    ) else { return false }
-    return currentDate() < maxAgeCache
-  }
-
-  private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
-    store.insert(feed.toLocal(), timestamp: currentDate()) { [weak self] error in
-      guard self != nil else { return }
-      completion(error)
     }
   }
 }
