706317dfcfc62447cbb1fbea6fb885086e8b94d1
Frederico Schnekenberg <fred.schnekl@gmail.com>
2024-11-27 11:21:04 -0300

    Break down 'CodableFeedStore tests to guarantee there's only one assertion per test. The goal is to clarify the behavior under test in small units, so we can extract the behavior tests into reusable specs. Create 'FeedStoreSpecs that must be implemented by any FeedStore test implementation to guarantee it meets spec Extract reusable 'FeedStoreSpecs helper methods into a shared scope so it can be used by other 'FeedStore* implementation tests

---
+++ EssentialFeed2Tests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -0,0 +1,23 @@
+//
+//  XCTestCase+FailableDeleteFeedStoreSpecs.swift
+//  EssentialFeed2Tests
+//
+//  Created by Frederico Schnekenberg on 27/11/24.
+//
+
+import XCTest
+import EssentialFeed2
+
+extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
+  func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+    let deletionError = deleteCache(from: sut)
+
+    XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+  }
+
+  func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+    deleteCache(from: sut)
+
+    expect(sut, toRetrieve: .empty, file: file, line: line)
+  }
+}
